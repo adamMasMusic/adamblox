220319d485c7b062a583a0d97f7d87bdb009583f
unknown <adammastera8@gmail.com>
2025-11-01 14:16:58 +0100

    Updated tampermonkey with correct link

--- main.user.js
+++ main.user.js
@@ -5,12 +5,12 @@
 // @description  Modern enhancements to Roblox home and creator dashboard
 // @match        https://www.roblox.com/*
 // @match        https://create.roblox.com/dashboard/creations*
-// @require      https://raw.githubusercontent.com/YourUsername/roblox-enhancer/main/ui.js
-// @require      https://raw.githubusercontent.com/YourUsername/roblox-enhancer/main/decals.js
+// @require      https://raw.githubusercontent.com/adamMasMusic/adamblox/refs/heads/main/ui.js
+// @require      https://raw.githubusercontent.com/adamMasMusic/adamblox/refs/heads/main/decals.js
 // @grant        GM_addStyle
 // ==/UserScript==
 
 (function () {
   'use strict';
-  console.log('Roblox Enhancer Loaded');
+  console.log('Adamblox Loaded');
 })();
